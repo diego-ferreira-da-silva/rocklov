848e3e7f8df16d6841afa0c2c6e41df865d513d9
Diego Ferreira <diego_fsilva92@hotmail.com>
2022-08-05 16:11:33 -0300

    Automação de todos os Steps do Cadastro

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -12,3 +12,43 @@ end
 Então('sou redirecionado para o Dashboard') do
     expect(page).to have_css ".dashboard"
 end
+
+Quando('submeto o meu cadastro sem o nome') do
+    find("#email").set Faker::Internet.free_email
+    find("#password").set "pwd123"
+    click_button "Cadastrar"
+end
+  
+Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eq "Oops. Informe seu nome completo!"
+end
+
+Quando('submeto o meu cadastro sem o email') do
+    find("#fullName").set "Diego Ferreira"
+    find("#password").set "pwd123"
+    click_button "Cadastrar"
+end
+
+Quando('submeto o meu cadastro com email incorreto') do
+    find("#fullName").set "Diego Ferreira"
+    find("#email").set "diego*gmail.com"
+    find("#password").set "pwd123"
+    click_button "Cadastrar"
+end
+  
+Então('vejos a mensagem de alerta: Oops. Informe um email válido!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eq "Oops. Informe um email válido!"
+  end
+
+Quando('submeto o meu cadastro sem a senha') do
+    find("#fullName").set "Diego Ferreira"
+    find("#email").set Faker::Internet.free_email
+    click_button "Cadastrar"
+end
+  
+Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
+    alert = find(".alert-dark")
+    expect(alert.text).to eq "Oops. Informe sua senha secreta!"
+end
